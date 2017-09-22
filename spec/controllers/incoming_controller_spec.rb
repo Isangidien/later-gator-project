4c1bb2a5d9fa793f2ac7196da414ba52b5b9ab8d
isangidien <i.inessa.miller@gmail.com>
2017-09-22 17:13:53 -0400

    added incoming_controller_spec, spec passed test.

---
+++ spec/controllers/incoming_controller_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe IncomingController, type: :controller do
+  describe "Get #create" do
+    it "returns http success" do
+      get :create
+      expect(response).to have_http_status(:success)
+    end
+  end
+end
